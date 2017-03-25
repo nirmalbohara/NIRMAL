f879e9e2e7fc77016ca043d6c4e5a9dce2aca0ff
nirmal <boharanirmal46@gmail.com>
2017-03-25 09:59:37 -0700

    add fibo2.py

---
+++ users.py
@@ -0,0 +1,10 @@
+user = {
+    'name': 'NIRMAL',
+    'email': 'nirmal@gmail.com',
+    'phone': '9848378273',
+    'address': 'NEPALGUNJ',
+}  
+
+
+for (key,val)  in user.items():
+	print('%s :%s' %(key,val)  )
